8b728c7d8e7864872276afbb83ae8dc8562ce507
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2025-10-20 05:50:46 +0200

    Improve handling of unsupported vacuums

--- custom_components/xiaomi_cloud_map_extractor/connector/vacuums/base/vacuum_v2.py
+++ custom_components/xiaomi_cloud_map_extractor/connector/vacuums/base/vacuum_v2.py
@@ -2,6 +2,7 @@ from abc import ABC
 from typing import Self
 
 from .vacuum_base import BaseXiaomiCloudVacuum
+from ...utils.dict_operations import path_extractor
 
 
 class BaseXiaomiCloudVacuumV2(BaseXiaomiCloudVacuum, ABC):
@@ -12,6 +13,15 @@ class BaseXiaomiCloudVacuumV2(BaseXiaomiCloudVacuum, ABC):
             "data": f'{{"obj_name":"{self._user_id}/{self._device_id}/{map_name}"}}'
         }
         api_response = await self._connector.execute_api_call_encrypted(url, params)
-        if api_response is None or "result" not in api_response or "url" not in api_response["result"]:
-            return None
-        return api_response["result"]["url"]
+        url = path_extractor(api_response, "result.url")
+        if url is None:
+            return await self.get_fallback_map_url(map_name)
+        return url
+
+    async def get_fallback_map_url(self: Self, map_name: str) -> str | None:
+        url = self._connector.get_api_url(self._server) + '/v2/home/get_interim_file_url_pro'
+        params = {
+            "data": f'{{"obj_name":"{self._user_id}/{self._device_id}/{map_name}"}}'
+        }
+        api_response = await self._connector.execute_api_call_encrypted(url, params)
+        return path_extractor(api_response, "result.url")
