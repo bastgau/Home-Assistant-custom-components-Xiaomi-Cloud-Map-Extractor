19806754ae8d82990886d151401098a65544731e
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2025-10-30 22:19:06 +0100

    Fix reauthentication

--- custom_components/xiaomi_cloud_map_extractor/connector/__init__.py
+++ custom_components/xiaomi_cloud_map_extractor/connector/__init__.py
@@ -92,7 +92,9 @@ class XiaomiCloudMapExtractorConnector:
 
         if not self._is_authenticated():
             _LOGGER.debug("Logging in...")
-            await self._cloud_connector.login_with_credentials(self._username, self._password)
+            if self._connector_config.username is None or self._connector_config.password is None:
+                raise FailedLoginException()
+            await self._cloud_connector.login_with_credentials(self._connector_config.username, self._connector_config.password)
             if not self._is_authenticated():
                 _LOGGER.error("Not authenticated!")
                 raise FailedLoginException()
